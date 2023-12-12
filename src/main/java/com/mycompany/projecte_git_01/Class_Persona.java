c713498d761120d52323aa3048906a6071b066eb
Teresa <mfolgue8@insalfonscostafreda.cat>
2023-12-12 11:48:46 +0100

    projecte finalitzat

--- src/main/java/com/mycompany/projecte_git_01/Class_Persona.java
+++ src/main/java/com/mycompany/projecte_git_01/Class_Persona.java
@@ -9,5 +9,22 @@ package com.mycompany.projecte_git_01;
  * @author HP
  */
 public class Class_Persona {
-    
+
+    private final String nom;
+    private final int edat;
+
+    // Constructor
+    public Class_Persona(String nom, int edat) 
+    {
+        this.nom = nom;
+        this.edat = edat;
+    }
+
+    // Mètode per saludar
+    public void saludar() {
+        System.out.println("Hola, soc en " + nom + " i tinc " + edat + " anys.");
+    }
+
+    // Altres mètodes o atributs poden ser afegits aquí.    
 }
+
--- src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
+++ src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
@@ -12,5 +12,11 @@ public class Projecte_GIT_01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        // Crea una instància de Class_Persona
+        Class_Persona Persona = new Class_Persona("Pere", 25);
+
+        // Crida el mètode saludar de NewClass
+        Persona.saludar();
     }
 }
